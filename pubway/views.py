d78c645a7ce335bfc9ffb2bf01a377a9f69e8a6f
Rodrigo Copetti <flipacholas@gmail.com>
2018-03-18 12:01:59 +0000

    Minor corrections

--- pubway/views.py
+++ pubway/views.py
@@ -102,7 +102,7 @@ def changepassword(request):
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
             messages.success(request, 'Your password was successfully updated!')
-            return redirect('change_password')
+            return redirect('index')
         else:
             messages.error(request, 'Please correct the error below.')
     else:
