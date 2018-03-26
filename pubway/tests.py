1d62c1e3bcc4db1df906ceaa583b84e79021a22d
Rodrigo Copetti <flipacholas@gmail.com>
2018-03-26 20:02:03 +0100

    Added test cases for users

--- pubway/tests.py
+++ pubway/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from pubway.models import UserProfile, User
+from django.contrib.auth import views as auth_views
+
+
+# Test case In the area of the user profile
+class UserManagementTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user("testUsername", "test@gmail.com", "testPassword")
+        UserProfile.objects.create(user=user)
+
+    def testUserExists(self):
+        # Test that the new user exists
+        user = User.objects.get(username="testUsername")
+        UserProfile.objects.get(user=user)
+
+        # Test that the number of users is 1
+        self.assertEqual(1, UserProfile.objects.count(), "Number of Profiles must be 1")
+
+    def testLogIn(self):
+        login = self.client.login(username='testUsername', password='testPassword')
+        self.assertTrue(login)
+
+    def testPasswordChange(self):
+        # Get created user
+        user = User.objects.get(username="testUsername")
+
+        # Test Change Password Form is found
+        response = self.client.get(reverse('changepassword'))
+        assert (response.status_code == 200 or response.status_code == 302)
+
+        # Test Password Change Form is validated
+        form = auth_views.PasswordChangeForm(user=user, data={
+            'old_password': 'testPassword',
+            'new_password1': 'newTestPassword',
+            'new_password2': 'newTestPassword'})
+        self.assertTrue(form.is_valid())
